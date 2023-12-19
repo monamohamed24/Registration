08765f7c8d6281f0561373db3a80b15a08bdb8a4
monamohamed24 <154070853+monamohamed24@users.noreply.github.com>
2023-12-19 14:07:34 +0200

    new feature

---
+++ src/main/java/Pages/ParentRegister/ParentAddInfoPage.java
@@ -0,0 +1,10 @@
+package Pages.ParentRegister;
+
+import Pages.PageBase;
+import org.openqa.selenium.WebDriver;
+
+public class ParentAddInfoPage extends PageBase {
+    public ParentAddInfoPage(WebDriver drive) {
+        super(drive);
+    }
+}
